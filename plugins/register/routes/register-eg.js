ea1941007f38bb8b70f438efcd59898b43524e5d
hamza <sghhamza10@gmail.com>
2020-12-03 12:22:15 +0100

    Add simple register in user level

--- plugins/register/routes/register-eg.js
+++ plugins/register/routes/register-eg.js
@@ -11,7 +11,32 @@ module.exports = function (gatewayExpressApp) {
   gatewayExpressApp.post('/api/register', async (req, res, next) => {
     try {
         console.log("*********************************",req.body)
+
+  
+        const {  firstname, lastname, email , phone , password, password_confirmation } = req.body
+        if (password != password_confirmation) {
+          throw new Error('password does not much')
+        }
+
+        myUser = await services.user.insert({
+          isActive: false,
+          firstname: firstname,
+          lastname: lastname,
+          username: email,
+          email: email,
+          phone: phone,
+          redirectUri: 'https://www.khallasli.com',
+        })
+  
+        crd_b = await services.credential.insertCredential(myUser.id, 'basic-auth', {
+          autoGeneratePassword: false,
+          password: password,
+          scopes: []
+        })
+
+        
         return res.status(200).json(req.body)
+
     } catch (err) {
       console.log('*************************************error', err);
       return res.status(422).json({ error: err.message })
