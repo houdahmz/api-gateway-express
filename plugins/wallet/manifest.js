ccc4ef79942c5e8ed6fc0868ac709b995f4309a4
hamza <sghhamza10@gmail.com>
2020-12-03 09:35:48 +0100

    add the new plugin as a policy

--- plugins/wallet/manifest.js
+++ plugins/wallet/manifest.js
@@ -5,7 +5,18 @@ module.exports = {
 
        pluginContext.registerPolicy(policy)
 
- 
+       pluginContext.eventBus.on('hot-reload', function ({ type, newConfig }) {
+        console.log('hot-reload', type, newConfig);
+      });
+      pluginContext.eventBus.on('http-ready', function ({ httpServer }) {
+        console.log('http ready');
+      });
+      pluginContext.eventBus.on('https-ready', function ({ httpsServer }) {
+        console.log('https ready');
+      });
+      pluginContext.eventBus.on('admin-ready', function ({ adminServer }) {
+        console.log('admin ready');
+      });  
       // pluginContext.registerGatewayRoute(app => { app.use(express.json()); 
 
     },
