61e78a1234d14d6f3cfeab9ab2b3e0e911d5157f
hamza <sghhamza10@gmail.com>
2020-12-04 17:19:08 +0100

    Add register route for admin scope

--- plugins/register/routes/register-eg.js
+++ plugins/register/routes/register-eg.js
@@ -69,13 +69,71 @@ module.exports = function (gatewayExpressApp) {
         return res.status(201).json({message:"Check your email : "+myUser.email});
 
     } catch (err) {
-      console.log('*************************************error', err);
+      return res.status(422).json({ error: err.message })
+    }
+  });
+
+  gatewayExpressApp.post('/api/completed-register', async (req, res, next) => {
+    try {
+
+        const {  firstname, lastname, email , phone , password, password_confirmation,typeId,commercial,city,zip_code,adresse,id_commercial,activity } = req.body
+        if (password != password_confirmation) {
+          throw new Error('password does not much')
+        }
+
+        myUser = await services.user.insert({ 
+          isActive: true, 
+          firstname: firstname,
+          lastname: lastname, 
+          username: email,
+          email: email,
+          phone: phone,
+          redirectUri: 'https://www.khallasli.com',
+        })
+        const creteProfile = async (myUser) => {
+          
+                  try {
+            return await axios.post('http://localhost:5000/api/profile', {
+              id_user:  myUser.id,
+              first_name: myUser.firstname,
+              last_name: myUser.lastname,
+              phone: myUser.phone,
+              typeId : typeId ,
+              commercial : commercial,
+              city : city ,
+              zip_code : zip_code ,
+              adresse : adresse , 
+              id_commercial: id_commercial,
+              activity : activity 
+            })
+          } catch (error) {
+            console.error(error)
+          }
+        }
+        crd_basic = await services.credential.insertCredential(myUser.id, 'basic-auth', {
+          autoGeneratePassword: false,
+          password: password,
+          scopes: ["admin"]  
+        })
+        //crd_oauth2 = await services.credential.insertCredential(myUser.id, 'oauth2')
+        const userProfile =  await creteProfile(myUser);
+      
+        // myProfile = await services.application.insert({
+        //   name: "complete_profile"+myUser.id,
+        //   redirectUri: 'http://localhost:5000/api/profile/'+userProfile.data.data.id
+        // },myUser.id)
+
+        const confirm_uri = "http://localhost:8080/oauth2/authorize?response_type=token&client_id="+myProfile.id+"&"+"redirect_uri="+myProfile.redirectUri  ;
+        console.log("url confirm : "+ confirm_uri);
+        mail.send_email("confirmation","confirmer votre profile svp \n "+ confirm_uri);
+        return res.status(201).json({user : myUser  , "profile" : userProfile});
+        
+    } catch (err) {
       return res.status(422).json({ error: err.message })
     }
 
 
   });
-
 
   
 
