d6689fcbcfac360e2879ebdbb12453038bd5766c
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2020-12-04 11:41:14 +0100

    Update

--- plugins/register/routes/register-eg.js
+++ plugins/register/routes/register-eg.js
@@ -2,11 +2,11 @@ const services = require('express-gateway/lib/services/')
 var bodyParser = require('body-parser')
 const superagent = require('superagent');
 const axios = require('axios');
-const mail = require("./mailer.config")
+const mail = require("./mailer.config");
+const { user } = require('express-gateway/lib/services/');
 
 module.exports = function (gatewayExpressApp) {
   gatewayExpressApp.use(bodyParser.json())
-
 
   gatewayExpressApp.post('/api/register', async (req, res, next) => {
     try {
@@ -18,15 +18,32 @@ module.exports = function (gatewayExpressApp) {
         }
 
         myUser = await services.user.insert({ 
-          isActive: true,
+          isActive: true, 
           firstname: firstname,
-          lastname: lastname,
+          lastname: lastname, 
           username: email,
           email: email,
           phone: phone,
           redirectUri: 'https://www.khallasli.com',
         })
-  
+        const creteProfile = async (myUser) => {
+          console.log("in create profile "+myUser.id)
+          console.log("in create profile "+myUser.firstname)
+          console.log("in create profile "+myUser.lastname)
+          console.log("in create profile "+myUser.phone)
+
+          try {
+            return await axios.post('http://localhost:5000/api/profile', {
+              id_user:  myUser.id,
+              first_name: myUser.firstname,
+              last_name: myUser.lastname,
+              phone: myUser.phone,
+              typeId: "1425d882-a03e-4ffc-b996-101233874443"
+            })
+          } catch (error) {
+            console.error(error)
+          }
+        }
         crd_basic = await services.credential.insertCredential(myUser.id, 'basic-auth', {
           autoGeneratePassword: false,
           password: password,
@@ -34,16 +51,22 @@ module.exports = function (gatewayExpressApp) {
         })
 
         crd_oauth2 = await services.credential.insertCredential(myUser.id, 'oauth2')
-       
+        const userProfile =  await creteProfile(myUser);
+        console.log("eeeeeeeeeeeeeeee",userProfile);
+
         myProfile = await services.application.insert({
           name: "complete_profile"+myUser.id,
-          redirectUri: 'http://localhost:5000/api/profile'
+          redirectUri: 'http://localhost:5000/api/profile/'+userProfile.data.data.id
         },myUser.id)
 
+        console.log("éazertt",userProfile.data)
         const confirm_uri = "http://localhost:8080/oauth2/authorize?response_type=token&client_id="+myProfile.id+"&"+"redirect_uri="+myProfile.redirectUri  ;
         console.log("url confirm : "+ confirm_uri);
         mail.send_email("confirmation","confirmer votre profile ssvp \n "+ confirm_uri);
-        return res.status(200).json(req.body)
+
+   
+
+        return res.status(201).json({message:"Check your email : "+myUser.email});
 
     } catch (err) {
       console.log('*************************************error', err);
@@ -52,6 +75,9 @@ module.exports = function (gatewayExpressApp) {
 
 
   });
+
+
+  
 
 
 
