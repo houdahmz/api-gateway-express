fa7353de137ca0296d7846202e9495c46e056bd3
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2020-12-10 10:48:17 +0100

    Clean code

--- plugins/bodyParser/policies/bodyParser.js
+++ plugins/bodyParser/policies/bodyParser.js
@@ -78,3 +78,41 @@ module.exports = {
    }
 };
 
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const swaggerUi = require("swagger-ui-express"),
+//      swaggerDocument = require("./docs/swagger.json");
+
+// const app = express();
+
+
+// app.use(cors(corsOptions));
+// app.use(bodyParser.json({limit: '50mb', extended: true}));
+// app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+// const { PassThrough } = require("stream");
+// const jsonParser = require("express").json();
+// const urlEncodedParser = require("express").urlencoded({ extended: true });
+
+// module.exports = {
+//   name: 'bodyParser',
+//   schema: {
+//     $id: 'N/A',
+//     type: 'object',
+//     properties: {}
+//   },
+//   policy: () => {
+//     return async (req, res, next) => {
+//       req.egContext.requestStream = new PassThrough();
+//       req.pipe(req.egContext.requestStream);
+//       return jsonParser(req, res, () => urlEncodedParser(req, res, next));
+//     };
+//   }
+// };
+
+// return async (req, res, next) => {
+
+//  };
