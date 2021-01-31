04e2bff5df1ceabda69635b034f2163060962385
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2021-01-31 23:47:24 +0100

    Add env config

---
+++ config/env.config.js
@@ -0,0 +1,11 @@
+const HTTP_PORT = process.env.HTTP_PORT || 8080;
+const JWT_SECRET = process.env.JWT_SECRET ;
+const HTTP_PORT_API_MANAGEMENT = process.env.HTTP_PORT_API_MANAGEMENT || 3000;
+
+module.exports = {
+    KEY: "uisfkskjebcio",
+    HTTP_PORT: HTTP_PORT,
+    JWT_SECRET: JWT_SECRET,
+    HTTP_PORT_API_MANAGEMENT: HTTP_PORT_API_MANAGEMENT,
+
+  };
