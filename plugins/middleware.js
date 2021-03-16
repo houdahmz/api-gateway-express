f84436a301e83cff5823cf3eda89c5236be3dbc9
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2021-03-16 14:52:08 +0100

    Update in middleware

--- plugins/middleware.js
+++ plugins/middleware.js
@@ -1,3 +1,5 @@
+const os = require('os');
+useragent = require('express-useragent');
 const middlewarePlugin = {
   schema: { $id: "./../config/models/schema.js" },
   version: '1.0.0',
@@ -11,6 +13,22 @@ const middlewarePlugin = {
           try {
             console.log("icii",req.body)
             let body = req.body;
+            var ip = (typeof req.headers['x-forwarded-for'] === 'string'
+            && req.headers['x-forwarded-for'].split(',').shift()) || 
+         req.connection.remoteAddress || 
+         req.socket.remoteAddress || 
+         req.connection.socket.remoteAddress
+         console.log("ip",ip)
+         console.log("os.platform()",os.platform())
+         console.log("os.release()",os.release())
+         console.log("os.type()",os.type()); // "Windows_NT"
+
+         var source = req.headers['user-agent']
+         var ua = useragent.parse(source);
+        //  console.log("ua",ua)
+         var isMobile = ua.isMobile
+         console.log("isMobile",isMobile)
+
                 if(body.user){
                 console.log("body before",body)
                 body.created_by = req.body.user.consumerId
