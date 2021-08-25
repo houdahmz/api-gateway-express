673b6b1571496f91b2589ed5a8459023db741d44
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2021-08-25 10:30:53 +0100

    Update registre

--- plugins/register/config.js
+++ plugins/register/config.js
@@ -1,13 +1,13 @@
 module.exports = { 
 
     CODE_ERROR : {
-        EMPTY : 00,
-        NOT_EXIST : 01,
-        TYPE : 02,
-        ALREADY_EXIST : 03,
-        REQUIRED: 04,
-        SERVER: 05,
-        INCONNU: 06,
+        EMPTY : 2000,
+        NOT_EXIST : 2001,
+        TYPE : 2002,
+        ALREADY_EXIST : 2003,
+        REQUIRED: 2004,
+        SERVER: 2005,
+        INCONNU: 2006,
 
     }
     ,
