aa25b4870f27e885c5de38e6d95757d4607d9719
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2021-10-13 13:51:35 +0100

    Update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const gateway = require('express-gateway');
 const swagger=require('swagger-express-jsdoc/app')
 
-const run =require('./run')
+// const run =require('./run')
 
 
 require('dotenv-safe').config({
