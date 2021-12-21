57bfae549f9ce4175424a05b119756b1317c76d2
dev-mobile <hassine.imen@eniso.u-sousse.tn>
2021-12-21 17:19:30 +0100

    Update validation yup

--- plugins/register/manifest.js
+++ plugins/register/manifest.js
@@ -9,10 +9,6 @@ module.exports = {
     pluginContext.registerGatewayRoute(require('./routes/handleUserStatus'));
     pluginContext.registerGatewayRoute(require('./routes/statistique'));
     pluginContext.registerGatewayRoute(require('./routes/user'));
-
-
-
-
   },
   policies: ['hello'], // this is for CLI to automatically add to "policies" whitelist in gateway.config
   schema: { // This is for CLI to ask about params 'eg plugin configure customer-auth'
