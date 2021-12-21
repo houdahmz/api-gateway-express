25a43cd24c3766703f4ee3eb41c8294a7c494c07
dev-mobile <hassine.imen@eniso.u-sousse.tn>
2021-12-21 16:09:09 +0100

    Update routes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const gateway = require('express-gateway');
-const swagger = require('swagger-express-jsdoc/app')
 
 require('dotenv-safe').config({
   allowEmptyValues: true,
